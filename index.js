22ebc42675922cd490f56671787bdf264695490b
c373 <76661778+c373@users.noreply.github.com>
2022-05-27 11:38:59 -0700

    Add event loading from different directories, fixes #1
    Placed event loading code into a function that can be called with a
    specific path to search for event listener files. Also greatly improved
    the logging at startup so it outputs to console the entire process of
    loading command and event files.

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const { Client, Collection, Intents } = require('discord.js');
 
 // load the unique, secure, super secret token from the config.json file...
 const { token } = require('../config.json');
+const {exit} = require('node:process');
 
 // create a new client instance, this represents our bot in code or bot is a client connecting to a discord server
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
@@ -25,6 +26,9 @@ client.commands = new Collection();
 
 // load files from the dir ./commands using filter
 // filter to only javascript files (ends with '.js')
+
+console.log("Loading commands...");
+
 const commandFiles = fs.readdirSync('../commands').filter(file => file.endsWith('.js'));
 
 // loop thru all the files
@@ -32,6 +36,7 @@ for (const file of commandFiles) {
 
 	// load the file (module) into command
 	const command = require(`../commands/${file}`);
+	console.log(`SUCCESS : ${file}`);
 
 	// set a new item in the collection
 	// with the command name as the key and the exported module (code to execute) as the value
@@ -46,36 +51,59 @@ for (const file of commandFiles) {
 
 // same exact concept as the section above that reads command files!
 // except for...
-
-const eventFiles = fs.readdirSync('../events').filter(file => file.endsWith('.js'));
-
-for (const file of eventFiles) {
-	const event = require(`../events/${file}`);
-
-	// THE FOLLOWING! We aren't loading commands we are creating event listeners...
-	// that means we are linking events, by name, to code that will be executed once the event occurs
-	// the name, whether it can run multiple times, and the code to execute are all contained in each file per event
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args));
+function loadEvents(client, path)
+{
+	if (!fs.existsSync(path))
+	{
+		console.log(`${path} not found, skipping...`)
+		return 1; // early exit if the specified path does not exist
 	}
 
-	/*	FURTHER EXPLANATION OF EVENT MODULES AND THEIR COMPONENTS
-		The event modules we are loading have a very specific structure to them
-		They all have three components (key/value pairs): name, once, and execute
+	const eventFiles = fs.readdirSync(path).filter(file => file.endsWith('.js'));
 
-			name -
-				this is where we specify the type of event by name that we are registering a function for
-				when an event occurs it notifies our bot of the type of event it is by name
-				if there is an event of this type registered with our bot (client) then it will call the appropriate function
+	for (const file of eventFiles) {
+		const event = require(`${path}/${file}`);
 
-			once -
-				a boolean(true/false) telling the bot whether it can respond to this type of event 
+		console.log(`SUCCESS : ${path}/${file}`)
 
-			execute -
-				the function that is to be executed upon this event occurring	*/
+		// THE FOLLOWING! We aren't loading commands we are creating event listeners...
+		// that means we are linking events, by name, to code that will be executed once the event occurs
+		// the name, whether it can run multiple times, and the code to execute are all contained in each file per event
+		if (event.once) {
+			client.once(event.name, (...args) => event.execute(...args));
+		} else {
+			client.on(event.name, (...args) => event.execute(...args));
+		}
+	}
+	
+	return 0;
 }
+
+/*	FURTHER EXPLANATION OF EVENT MODULES AND THEIR COMPONENTS
+The event modules we are loading have a very specific structure to them
+They all have three components (key/value pairs): name, once, and execute
+
+name -
+this is where we specify the type of event by name that we are registering a function for
+when an event occurs it notifies our bot of the type of event it is by name
+if there is an event of this type registered with our bot (client) then it will call the appropriate function
+
+once -
+a boolean(true/false) telling the bot whether it can respond to this type of event 
+
+execute -
+the function that is to be executed upon this event occurring	*/
+
+console.log('Loading events...')
+loadEvents(client, '../events');
+
+if (loadEvents(client, './events') != 0)
+{
+	console.log(`FATAL: './events' not found unable to continue, exiting...`);
+	exit();
+}
+
+
 
 // login to Discord with your client's token
 client.login(token);
